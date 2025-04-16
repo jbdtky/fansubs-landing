3c4f79e838aad6fb0ba93a7f35b9d41f1f06c89d
nikitaabramov.dev <abramov.mykyta@gmail.com>
2025-04-16 12:13:01 +0300

    🚑️ Change emojis

--- src/components/sections/Screen.jsx
+++ src/components/sections/Screen.jsx
@@ -17,59 +17,64 @@ export const Screen = () => {
     });
   }, []);
 
-  const options = useMemo(
-    () => ({
-      fullScreen: { enable: false },
-      background: { color: "transparent" },
-      fpsLimit: 60,
-      particles: {
-        shape: {
-          type: "emoji",
-          options: {
-            emoji: {
-                value: ["💕", "💸", "💰", "🤩"],
+  const options = {
+    fullScreen: { enable: false },
+    background: { color: "transparent" },
+    fpsLimit: 60,
+    particles: {
+      shape: {
+        type: "image",
+        options: {
+          image: [
+            {
+              src: "/assets/emojis/heart.png", // 💕
+              width: 32,
+              height: 32,
             },
-          },
-        },
-        size: {
-          value: 14, 
-          random: false, 
-        },
-        move: {
-          enable: true,
-          speed: { min: 1, max: 3 },
-          outModes: { default: "bounce" },
-          direction: "out",
-        },
-        number: {
-         value: isMobile ? 20 : 60,
-          // density: { enable: true, area: 800 },
-        },
-        opacity: { value: { min: 0.5, max: 1 } }, 
-        position: {
-          x: 50,
-          y: 50,
-        },
-        render: {
-          fill: true, 
-          stroke: {
-            width: 0, 
-            color: "transparent",
-          },
+            {
+              src: "/assets/emojis/money-fly.png", // 💸
+              width: 32,
+              height: 32,
+            },
+            {
+              src: "/assets/emojis/money-bag.png", // 💰
+              width: 32,
+              height: 32,
+            },
+            {
+              src: "/assets/emojis/star-eyes.png", // 🤩
+              width: 32,
+              height: 32,
+            },
+          ],
         },
       },
-      interactivity: {
-        events: {
-          onHover: { enable: true, mode: "repulse" },
-        },
-        modes: {
-          repulse: { distance: 100, duration: 0.4 },
-        },
+      size: {
+        value: 20,
+        random: false,
       },
-      detectRetina: true,
-    }),
-    []
-  );
+      move: {
+        enable: true,
+        speed: { min: 1, max: 3 },
+        outModes: { default: "bounce" },
+        direction: "none",
+      },
+      number: {
+        value: window.innerWidth < 768 ? 20 : 60,
+      },
+      opacity: { value: { min: 0.5, max: 1 } },
+    },
+    interactivity: {
+      events: {
+        onHover: { enable: true, mode: "repulse" },
+      },
+      modes: {
+        repulse: { distance: 100, duration: 0.4 },
+      },
+    },
+    detectRetina: true,
+  };
+  
 
   return (
     <section className="relative mb-[160px] sm:mb-[110px]">
