01ee01d9b408c16536b56e03e311efafa221635e
Jean-Baptiste Dominguez <jeanbaptiste.dominguez@gmail.com>
2025-05-01 11:00:09 -0700

    few fixes

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,7 @@
-import { useEffect } from "react";
 import { Navbar } from "./elements/Navbar";
 import { Footer } from "./elements/Footer";
 
 export const Layout = ({ title, children }) => {
-  useEffect(() => {
-    document.title = title ?? "FanSubs: Easy Earnings. Happy Fans.";
-  }, [title]);
-
   return (
     <>
       <Navbar />
